7299878b507f7951e893a01fe783f0478d137ea6
emmanuel-carreira <emmanuel_carreira@hotmail.com>
2020-04-02 21:14:31 -0300

    refactor: add not null constraint to table columns

--- backend/src/database/migrations/20200402202759_create_water.js
+++ backend/src/database/migrations/20200402202759_create_water.js
@@ -1,9 +1,9 @@
 exports.up = function(knex) {
   return knex.schema.createTable('water', (table) => {
     table.increments('id');
-    table.string('userId');
-    table.decimal('quantity', 5, 3);
-    table.date('date');
+    table.string('userId').notNullable();
+    table.decimal('quantity', 5, 3).notNullable();
+    table.date('date').notNullable();
 
     table.foreign('userId').references('id').inTable('user');
   });
