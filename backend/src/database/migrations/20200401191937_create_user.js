144cdb6ea45bbd76812fb8341ed082538a846ba2
emmanuel-carreira <emmanuel_carreira@hotmail.com>
2020-04-04 18:40:53 -0300

    fix: add unique constraint to username and email
    1. Add unique constraint to username and email columns in user table

--- backend/src/database/migrations/20200401191937_create_user.js
+++ backend/src/database/migrations/20200401191937_create_user.js
@@ -1,9 +1,9 @@
 exports.up = function (knex) {
   return knex.schema.createTable('user', (table) => {
     table.increments('id');
-    table.string('username').notNullable();
+    table.string('username').notNullable().unique();
     table.string('password').notNullable();
-    table.string('email').notNullable();
+    table.string('email').notNullable().unique();
     table.boolean('active').notNullable();
   });
 };
