44e6b4a82d8dbcaff667bab86db6265f72cef49a
FernandoCCNeto <58184811+FernandoCCNeto@users.noreply.github.com>
2020-04-03 21:58:33 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
+const UserController = require('./controllers/UserController');
+
 const routes = express.Router();
+
+routes.post('/createUser', UserController.create);
+routes.delete('/deleteUser/:id', UserController.delete);
+routes.get('/getUsers', UserController.index);
 
 module.exports = routes;
