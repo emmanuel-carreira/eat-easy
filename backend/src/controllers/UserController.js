6c913e81932e3a87fa9ce10431ad04b895d7bcaf
FernandoCCNeto <58184811+FernandoCCNeto@users.noreply.github.com>
2020-04-06 19:03:33 -0300

    Update UserController.js
    Changes to improve the code smell.

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,42 +1,42 @@
 const connection = require('../database/connection');
 
 module.exports = {
-    async index(request, response) {
-        const user = await connection('user').select('*');
+  async index (request, response) {
+    const user = await connection('user').select('*');
     
-        return response.json(user);
+    return response.json(user);
+  },
+
+  async create (request, response) {
+    const { username, password, email, active } = request.body;
+
+    try { 
+      await connection('user').insert({
+        username,
+        password,
+        email,
+        active
+      });
+      } catch (exception) {
+        console.log('Exception: ' + exception);
+        return response.status(400).send();
+      }
+
+      return response.status(200).send();
     },
 
-    async create(request, response){
-        const { username, password, email, active } = request.body;
+    async delete (request, response) {
+      const { id } = request.params;
 
-        try { 
-            await connection('user').insert({
-                username,
-                password,
-                email,
-                active
-            })
-        } catch (exception) {
-            console.log('Exception: ' + exception);
-            return response.status(400).send();
-        }
-
-        return response.status(200).send();
-    },
-
-    async delete(request, response){
-        const { id } = request.params;
-
-        try { 
-            await connection('user').where('id', '=' , id).update({
-                active : false
-            }) 
-        } catch (exception) {
-            console.log('Exception: ' + exception);
-            return response.status(400).send();
-        }
+      try { 
+        await connection('user').where('id', '=' , id).update({
+          active : false
+        }); 
+      } catch (exception) {
+        console.log('Exception: ' + exception);
+        return response.status(400).send();
+      }
         
-        return response.status(200).send();
+      return response.status(200).send();
     }
 }   
