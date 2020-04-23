a6303ffb0ba715a47dce21f3173a977c25923544
emmanuel-carreira <emmanuel_carreira@hotmail.com>
2020-04-23 18:17:09 -0300

    feat: Add water creation route

---
+++ backend/src/controllers/WaterController.js
@@ -0,0 +1,16 @@
+const connection = require('../database/connection');
+
+module.exports = {
+  async create (request, response) {
+    const { userId, quantity, date } = request.body;
+
+    try {
+      await connection('water').insert({ userId, quantity, date })
+    } catch (exception) {
+      console.log('Exception: ' + exception);
+      return response.status(400).send();
+    }
+
+    return response.status(204).send();
+  }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const UserController = require('./controllers/UserController');
+const WaterController = require('./controllers/WaterController');
 
 const routes = express.Router();
 
 routes.post('/createUser', UserController.create);
 routes.delete('/deleteUser/:id', UserController.delete);
 routes.get('/getUsers', UserController.index);
+
+routes.post('/createWater', WaterController.create);
 
 module.exports = routes;
