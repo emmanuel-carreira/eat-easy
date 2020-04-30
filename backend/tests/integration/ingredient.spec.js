c662d41cae965b2f84c8d518c086282909f8c35e
emmanuel-carreira <emmanuel_carreira@hotmail.com>
2020-04-30 18:16:04 -0300

    tests: Add integration tests to get and list ingredients routes

--- backend/tests/integration/ingredient.spec.js
+++ backend/tests/integration/ingredient.spec.js
@@ -36,4 +36,58 @@ describe('ingredient', () => {
 
     expect(response.status).toBe(400);
   });
+
+  it('should return data for a specific ingredient', async () => {
+    const response = await request(app).get('/getIngredient/1').send();
+
+    expect(response.status).toBe(200);
+
+    const ingredient = response.body;
+    expect(ingredient.grams).toBe(100);
+    expect(ingredient.calories).toBe(349.86);
+    expect(ingredient.protein).toBe(14.59);
+    expect(ingredient.carbohydrates).toBe(20);
+    expect(ingredient.fats).toBe(23.5);
+  });
+
+  it('should NOT return data for an inexistent ingredient', async () => {
+    const response = await request(app).get('/getIngredient/5').send();
+
+    expect(response.status).toBe(200);
+
+    const ingredient = response.body;
+    expect(ingredient).toBe('');
+  });
+
+  it('should return a list with the first ingredients', async () => {
+
+    // Register a second ingredient
+    await request(app).post('/createIngredient').send({
+      grams: "30",
+      protein: "10",
+      carbohydrates: "5",
+      fats: "15"
+    });
+
+    const response = await request(app).get('/listIngredient').send();
+
+    expect(response.status).toBe(200);
+
+    const ingredients = response.body;
+    expect(ingredients.length).toBe(2);
+
+    const firstIngredient = ingredients[0];
+    expect(firstIngredient.grams).toBe(100);
+    expect(firstIngredient.calories).toBe(349.86);
+    expect(firstIngredient.protein).toBe(14.59);
+    expect(firstIngredient.carbohydrates).toBe(20);
+    expect(firstIngredient.fats).toBe(23.5);
+
+    const secondIngredient = ingredients[1];
+    expect(secondIngredient.grams).toBe(30);
+    expect(secondIngredient.calories).toBe(195);
+    expect(secondIngredient.protein).toBe(10);
+    expect(secondIngredient.carbohydrates).toBe(5);
+    expect(secondIngredient.fats).toBe(15);
+  });
 });
